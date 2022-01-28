5b4fb6c595d26a53f9a7667bb93a08f3c9c3245a
Julia Barcelos <julaai@gmail.com>
2022-01-28 18:41:50 -0300

    Criamos os itens do searchBar, 3 radios e 1 input. Req 12, 13

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,41 @@ import React from 'react';
 function SearchBar() {
   return (
     <div>
-      <h1> search bar </h1>
+      <input
+        type="text"
+        name="search"
+        data-testid="search-input"
+        placeholder="Search something"
+      />
+      <div>
+        <label htmlFor="ingredient">
+          <input
+            id="ingredient"
+            name="searchFilter"
+            data-testid="ingredient-search-radio"
+            type="radio"
+          />
+          Ingredients
+        </label>
+        <label htmlFor="name">
+          <input
+            id="name"
+            name="searchFilter"
+            data-testid="name-search-radio"
+            type="radio"
+          />
+          Name
+        </label>
+        <label htmlFor="firstLetter">
+          <input
+            id="firstLetter"
+            name="searchFilter"
+            data-testid="first-letter-search-radio"
+            type="radio"
+          />
+          First Letter
+        </label>
+      </div>
     </div>
   );
 }
