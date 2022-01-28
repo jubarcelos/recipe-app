4d92025e94e830808dd9875d824774107c8615a3
Julia Barcelos <julaai@gmail.com>
2022-01-28 18:22:47 -0300

    Envolvemos a imagem de busca em um botao e criamos a lãgica de toggle para exibir ou nao o searchBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProfileIcon from '../images/profileIcon.svg';
 import SearchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header() {
+  const [showBar, setShowBar] = useState(false);
+
+  const toggleSearchBar = () => (
+    setShowBar((preventState) => !preventState)
+  );
+
   return (
     <header>
       <Link to="/profile">
@@ -14,13 +21,22 @@ function Header() {
         />
       </Link>
       <h1 data-testid="page-title"> Title </h1>
-      <img
-        data-testid="search-top-btn"
-        src={ SearchIcon }
-        alt="iconSearch"
-      />
+      <button
+        type="button"
+        onClick={ toggleSearchBar }
+      >
+        <img
+          data-testid="search-top-btn"
+          src={ SearchIcon }
+          alt="iconSearch"
+        />
+      </button>
+      {
+        showBar && <SearchBar />
+      }
     </header>
   );
 }
 
+// linha 35 tem que ir pra página principal de receitas
 export default Header;
