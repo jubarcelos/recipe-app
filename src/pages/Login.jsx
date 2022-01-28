6d9930ef402395cc7e3cffb898b4e19caa78c766
Christian Lessa <christianclessa@gmail.com>
2022-01-28 16:57:43 -0300

    Req 2 a 4 - Criando imputs Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,35 @@ import React from 'react';
 
 function Login() {
   return (
-    
+    <form>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="text"
+          data-testid="email-input"
+          name="email"
+          id="email"
+          placeholder="Digite seu Email"
+        />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input
+          data-testid="password-input"
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Digite sua senha"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </form>
   );
 }
+
+export default Login;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -17,6 +17,7 @@
 
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Login from '../pages/Login';
 
 function Routes() {
   return (
