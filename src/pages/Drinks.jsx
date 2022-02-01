497234b9fe80c8e1eee9538abb9ccdc03a500275
Julia Barcelos <julaai@gmail.com>
2022-02-01 17:21:53 -0300

    Inclusao da lógica de redirecionar para a pagina de detalhes caso o retorno da API seja de apenas uma receita. Req 16.

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import { drinkContext } from '../context';
 
 function Drinks() {
+  const { drinkRecipes } = useContext(drinkContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (drinkRecipes.length === 1) {
+      const drinkId = drinkRecipes[0].idDrink;
+      history.push(`/drinks/${drinkId}`);
+    }
+  }, [drinkRecipes, history]);
+
   return (
     <div>
       <Header title="Drinks" />
-      <h1> opa to no drinks </h1>
     </div>
   );
 }
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import { foodContext } from '../context';
 
 function Foods() {
+  const { foodRecipes } = useContext(foodContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (foodRecipes.length === 1) {
+      const foodId = foodRecipes[0].idMeal;
+      history.push(`/foods/${foodId}`);
+    }
+  }, [foodRecipes, history]);
+
   return (
     <div>
       <Header title="Foods" />
