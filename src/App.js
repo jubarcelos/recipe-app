6938eab56470986c4654d716d7bc1a70d52d05b1
Angelica <angelicapedroso05@gmail.com>
2022-02-01 00:45:59 -0300

    corrige lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-  );
+  return (<div />);
 }
 
 export default App;
