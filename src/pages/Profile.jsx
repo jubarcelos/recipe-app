ec25d3a480128719d452f87e1c1528cb106b6056
Angelica <angelicapedroso05@gmail.com>
2022-02-01 17:11:49 -0300

    adiciona informacoes da tela de perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
-import Header from '../components/Header';
+import { useHistory } from 'react-router-dom';
 
 function Profile() {
+  const history = useHistory();
+  const { email } = JSON.parse(localStorage.getItem('user'));
+
+  const handleClick = () => {
+    history.push('/');
+    localStorage.clear();
+  };
+
   return (
-    <Header title="Profile" />
+    <div>
+      <h3
+        data-testid="profile-email"
+      >
+        {email}
+      </h3>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ handleClick }
+      >
+        Logout
+      </button>
+    </div>
   );
 }
 
