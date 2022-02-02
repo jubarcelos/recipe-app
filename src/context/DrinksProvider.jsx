c18027212b363b54caf94462a30c4e1fd333c005
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-02-02 16:58:07 -0300

    Seta filtro inicial nas pages foods e drinks

--- src/context/DrinksProvider.jsx
+++ src/context/DrinksProvider.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { drinkContext } from '.';
 
 function DrinksProvider({ children }) {
   const [drinkRecipes, setDrinkRecipes] = useState([]);
+
+  const setInitialRecipes = async () => {
+    const firstRecipes = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    const JSON = await firstRecipes.json();
+    const ELEVEN = 11;
+    const twoelvRecipes = JSON.drinks.filter((__, index) => index <= ELEVEN);
+    setDrinkRecipes(twoelvRecipes);
+  };
+
+  useEffect(() => {
+    setInitialRecipes();
+  }, []);
 
   return (
     <drinkContext.Provider value={ { drinkRecipes, setDrinkRecipes } }>
--- src/context/FoodsProvider.jsx
+++ src/context/FoodsProvider.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { foodContext } from '.';
 
 function FoodsProvider({ children }) {
   const [foodRecipes, setFoodRecipes] = useState([]);
+
+  const setInitialRecipes = async () => {
+    const firstRecipes = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const JSON = await firstRecipes.json();
+    const ELEVEN = 11;
+    const twoelvRecipes = JSON.meals.filter((__, index) => index <= ELEVEN);
+    setFoodRecipes(twoelvRecipes);
+  };
+
+  useEffect(() => {
+    setInitialRecipes();
+  }, []);
 
   return (
     <foodContext.Provider value={ { foodRecipes, setFoodRecipes } }>
