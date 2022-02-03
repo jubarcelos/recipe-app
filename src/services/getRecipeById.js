a7d75726b638b4c5b700aa1486f7b6fd70f7222e
Julia Barcelos <julaai@gmail.com>
2022-02-03 18:11:05 -0300

    Modificamos o retorno da API para direto o array.

--- src/services/getRecipeById.js
+++ src/services/getRecipeById.js
@@ -2,11 +2,13 @@ const getRecipeById = (pathname, id) => {
   switch (pathname) {
   case `/foods/${id}`:
     return fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-      .then((response) => response.json());
+      .then((response) => response.json())
+      .then((data) => data.meals);
   case `/drinks/${id}`:
     return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
-      .then((response) => response.json());
-  default: return 'oi';
+      .then((response) => response.json())
+      .then((data) => data.drinks);
+  default: return 'null';
   }
 };
 export default getRecipeById;
