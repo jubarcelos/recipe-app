ee9f56ab51ff553816a6ac2cdfd938c1063973da
Julia Barcelos <julaai@gmail.com>
2022-02-03 18:12:46 -0300

    Implementamos as lógicas comuns de renderizacao dos componentes  drinkDetails  e foodDetails.

--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -1,52 +1,63 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-// import shareRecipe from '../images/shareIcon.svg';
-// import favoriteRecipe from '../images/whiteHeartIcon.svg';
 import getRecipeById from '../services/getRecipeById';
+import FoodDetails from './FoodDetails';
+import DrinkDetails from './DrinkDetails';
 
 function DetailsCard() {
+  const [actualRecipe, setActualRecipe] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  console.log(actualRecipe);
+
   const history = useHistory();
   const { location: { pathname } } = history;
   const { id } = useParams();
 
   useEffect(() => {
-    const test = async () => {
-      const datas = await getRecipeById(pathname, id);
-      console.log(datas);
+    const selectRecipe = async () => {
+      const selectedRecipe = await getRecipeById(pathname, id);
+      setActualRecipe(selectedRecipe[0]);
+      setIsLoading(true);
     };
-    test();
+    selectRecipe();
   }, [pathname, id]);
 
+  const actualRecipeArray = Object.entries(actualRecipe);
+
+  const ingredients = actualRecipeArray.filter((item) => item[0]
+    .includes('strIngredient') && item[1] !== null);
+
+  const measure = actualRecipeArray.filter((item) => item[0]
+    .includes('strMeasure') && item[1] !== null);
+
+  const renderRecipeDetails = (datas) => (
+    datas.map((data, index) => (
+      <p
+        key={ data[0] }
+        data-testid={ `${index}-ingredient-name-and-measure` }
+      >
+        {
+          measure[index] ? `${measure[index][1]} - ${data[1]}`
+            : data[1]
+        }
+      </p>
+    )));
+
   return (
-    <h1>
-      oi
-    </h1>
-    // <div>
-    //   <img
-    //     data-testid="recipe-photo"
-    //     src={ recipeImage }
-    //     alt="recipe"
-    //   />
-    //   <div>
-    //     <h1 data-testid="recipe-title">
-    //       { recipeTitle }
-    //     </h1>
-    //     <button type="button">
-    //       { shareRecipe }
-    //     </button>
-    //     <button type="button">
-    //       { favoriteRecipe }
-    //     </button>
-    //   </div>
-    //   <p data-testid="recipe-category">{ category }</p>
-    //   <h2 data-testid="${index}-ingredient-name-and-measure"> oi </h2>
-    //   <p data-testid="instructions"> oi</p>
-    //   <video data-testid="video" src={ recipeVideo } controls>
-    //     <track kind="captions" />
-    //     O seu navegador não suporta o elemento
-    //     <code>video</code>
-    //   </video>
-    // </div>
+    <div>
+      { isLoading
+        && (
+          pathname === `/foods/${id}` ? <FoodDetails
+            actualRecipe={ actualRecipe }
+            renderRecipeDetails={ renderRecipeDetails }
+            ingredients={ ingredients }
+          /> : <DrinkDetails
+            actualRecipe={ actualRecipe }
+            renderRecipeDetails={ renderRecipeDetails }
+            ingredients={ ingredients }
+          />
+        )}
+    </div>
   );
 }
 
