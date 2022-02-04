5f8a282b620fe668f52b6b8dff7b2e1fb2988b27
Julia Barcelos <julaai@gmail.com>
2022-02-03 22:49:13 -0300

    inicia lógica de renderizacao, mas está dando problemas e por isso deixei comentado para continuar depois.

--- src/components/Carrossel.jsx
+++ src/components/Carrossel.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import getRecommendation from '../services/getRecommendation';
+// import { userContext } from '../context';
 
 function Carrossel() {
-  const [actualRecommendation, setActualRecommendation] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  // const [recommendations, setRecommendations] = useContext(userContext);
 
   const history = useHistory();
   const { location: { pathname } } = history;
@@ -15,16 +16,42 @@ function Carrossel() {
       const FIVE = 5;
       const renderRecommendation = await getRecommendation(pathname, id);
       const sixRecipes = renderRecommendation.filter((_recipe, index) => index <= FIVE);
-      setActualRecommendation(sixRecipes);
+      setRecommendations(sixRecipes);
       setLoading(true);
     };
     recipeRecommendation();
   }, [pathname, id]);
 
+  // const drinkRecommendationsArray = Object.entries(drinkRecommendations);
+  // const foodRecommendationsArray = Object.entries(foodRecommendations);
+
+  // const recommendationCard = (recipes) => {
+  //   recipes.map((recipe, index) => (
+  //     <div key={ index } data-testid={ `${index}-recipe-card` }>
+  //       <img
+  //         data-testid={ `${index}-card-img` }
+  //         src={ recipe.strThumbDrink }
+  //         alt="recipeImage"
+  //         width="80"
+  //       />
+  //       <p
+  //         data-testid={ `${index}-card-name` }
+  //       >
+  //         { recipe.strDrink }
+  //         será que veio?
+  //       </p>
+  //     </div>
+  //   ));
+  // };
+
   return (
     <div>
       <h1>Carrossel</h1>
-      { loading && console.log(actualRecommendation) }
+      {/* {
+        loading && pathname === `/foods/${id}`
+          ? recommendationCard(drinkRecommendationsArray)
+          : recommendationCard(foodRecommendationsArray)
+      } */}
     </div>
   );
 }
