acc3024f7effe419d1897e207ea2462c385aeffd
Julia Barcelos <julaai@gmail.com>
2022-02-04 18:49:30 -0300

    inicia implementacao de DoneRecipe e Progress Recipe e paramos para focar nos requisots em ordem

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+// import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
+// import { setDoneRecipe } from '../services/localStorage';
 
 function DoneRecipes() {
+  // const [recipes, setRecipes] = useState([]);
+  // const { id } = useParams();
+
+  // useEffect(() => {
+  //   setRecipes(id);
+  //   console.log(recipes);
+  // });
   return (
     <div>
       <Header title="Done Recipes" />
       <h1>done</h1>
+      {/* { setDoneRecipe(recipes) } */}
     </div>
   );
 }
--- src/pages/ProgressDrinks.jsx
+++ src/pages/ProgressDrinks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { setInProgressRecipe } from '../services/localStorage';
 
 function ProgressDrinks() {
   return (
