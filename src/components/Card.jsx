80eb2f75dc23180ff0ad32a2885d2f34c9a43d1a
Julia Barcelos <julaai@gmail.com>
2022-02-09 11:19:29 -0300

    Arruma rota do food nationalits para ir direto ao details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function Card({ index, recipeImage, recipeName, id }) {
-  const card = (
-    <div data-testid={ `${index}-recipe-card` }>
-      <img
-        data-testid={ `${index}-card-img` }
-        src={ recipeImage }
-        alt="recipeImage"
-        width="80"
-      />
-      <p
-        data-testid={ `${index}-card-name` }
-      >
-        { recipeName }
-      </p>
-    </div>
-  );
-
-  const history = useHistory();
-  const { location: { pathname } } = history;
+function Card({ index, recipeImage, recipeName, id, type }) {
   return (
     <div>
-      {
-        pathname === '/foods'
-          ? <Link to={ `${pathname}/${id}` }>{ card }</Link>
-          : <Link to={ `drinks/${id}` }>{ card }</Link>
-      }
+      <Link
+        to={ {
+          pathname: `/${type}s/${id}`,
+        } }
+        data-testid={ `${index}-recipe-card` }
+      >
+        <img
+          data-testid={ `${index}-card-img` }
+          src={ recipeImage }
+          alt="recipeImage"
+          width="80"
+        />
+        <p
+          data-testid={ `${index}-card-name` }
+        >
+          { recipeName }
+        </p>
+      </Link>
     </div>
-
   );
 }
 
@@ -40,4 +34,5 @@ Card.propTypes = {
   recipeName: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
   id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
