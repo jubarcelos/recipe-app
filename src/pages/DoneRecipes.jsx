35fbffa0011ed7b8669d853000febdf6e669134e
Julia Barcelos <julaai@gmail.com>
2022-02-10 11:58:43 -0300

    Retira a chamada dos bottoes daqui e cola em CardDone

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import CardDone from '../components/CardDone';
-import ButtonFilters from '../components/ButtonFilters';
 import { getLocalStorageInfo } from '../services/localStorage';
 
 function DoneRecipes() {
   const allDoneRecipes = getLocalStorageInfo('doneRecipes');
-
   return (
     <div>
       <Header title="Done Recipes" />
-      <ButtonFilters allDoneRecipes={ allDoneRecipes } />
-      <CardDone allDoneRecipes={ allDoneRecipes } />
+      <CardDone allRecipes={ allDoneRecipes } />
     </div>
   );
 }
