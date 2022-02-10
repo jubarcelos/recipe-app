cf58d46da52d79245f37eb017b5f0a16ea7b3329
Julia Barcelos <julaai@gmail.com>
2022-02-10 11:54:24 -0300

    modifica a forma como os componentes sa2o chamados e onde

--- src/components/CardDone.jsx
+++ src/components/CardDone.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
+import PropTypes from 'prop-types';
 import ShareDoneRecipe from './ShareDoneRecipe';
+import ButtonFilters from './ButtonFilters';
 
-function CardDone() {
+function CardDone({ allRecipes }) {
+  const [actualFiltered, setActualFiltered] = useState(allRecipes);
   const history = useHistory();
   const redirect = (type, id) => (
     history.push(`/${type}s/${id}`)
   );
   return (
     <div>
+      <ButtonFilters
+        setActualFiltered={ setActualFiltered }
+        allRecipes={ allRecipes }
+      />
+
       {
-        allDoneRecipes.length > 0 && allDoneRecipes.map((recipe, index) => (
+        actualFiltered.length > 0 && actualFiltered.map((recipe, index) => (
           <div key={ index }>
             <ShareDoneRecipe index={ index } recipe={ recipe } />
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              { recipe.type === 'food'
+                ? `${recipe.nationality} - ${recipe.category}`
+                : recipe.alcoholicOrNot }
+
+            </p>
             <button
               type="button"
               onClick={ () => redirect(recipe.type, recipe.id) }
@@ -26,6 +40,7 @@ function CardDone() {
             </button>
             <button
               type="button"
+              onClick={ () => redirect(recipe.type, recipe.id) }
             >
               <h1
                 data-testid={ `${index}-horizontal-name` }
@@ -34,26 +49,28 @@ function CardDone() {
               </h1>
             </button>
 
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              { recipe.type === 'meal' ? recipe.category : recipe.alcoholicOrNot }
-            </p>
             <p data-testid={ `${index}-horizontal-done-date` }>{ recipe.doneDate }</p>
             {
-              recipe.tags.map((tag, i) => (
-                <button
-                  key={ i }
-                  type="button"
-                  data-testid={ `${index}-${tag}-horizontal-tag` }
-                >
-                  { tag }
-                </button>
-              ))
+              recipe.tags.length > 0
+                && recipe.tags.map((tag, i) => (
+                  <button
+                    key={ i }
+                    type="button"
+                    data-testid={ `0-${tag}-horizontal-tag` }
+                  >
+                    { tag }
+                  </button>
+                ))
             }
           </div>
         ))
       }
     </div>
-  )
+  );
 }
 
 export default CardDone;
+
+CardDone.propTypes = {
+  allRecipes: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
